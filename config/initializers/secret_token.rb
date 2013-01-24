d3b350231ad6c3efc46d312d1fe4d9e4d8340efd
TheOtherZach <briggszj@gmail.com>
2013-01-24 18:40:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SucksRocks::Application.config.secret_token = 'ad1dfec5f6a9cc4566811e8122c1ba82d4af1211de99874de6dc6e20090c5be5d571d3bdde39cd12804175ea1e9d5a0a322831dc60dc2fca1dd21e4f64bed6a0'
