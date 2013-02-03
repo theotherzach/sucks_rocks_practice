713e4683822768212aab85756b4c8be16acf368c
TheOtherZach <briggszj@gmail.com>
2013-02-03 15:48:10 -0500

    fix bug searches were not quoted

---
+++ features/step_definitions/search.rb
@@ -0,0 +1,8 @@
+When /^I search for (.*)$/ do |term|
+  @scores ||= {}
+  @scores[term] = RockScore.for_term(term)
+end
+
+Then /^the beatles should have a higher score than comcast$/ do
+  @scores["the beatles"].should be > @scores["comcast"]
+end
--- features/step_definitions/search_steps.rb
+++
@@ -1,8 +0,0 @@
-When /^I search for (.*)$/ do |term|
-  @scores ||= {}
-  @scores[term] = RockScore.for_term(term)
-end
-
-Then /^apple should have a higher score than microsoft$/ do
-  @scores["apple"].should be > @scores["microsoft"]
-end
