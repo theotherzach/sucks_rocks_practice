c078dac67a32c3d0e3349f13b589baae5cbe99e9
TheOtherZach <briggszj@gmail.com>
2013-02-09 17:40:13 -0500

    NoScore now converted to nil in JSON

--- features/step_definitions/search.rb
+++ features/step_definitions/search.rb
@@ -1,6 +1,8 @@
 When /^I search for (.*)$/ do |term|
   @scores ||= {}
-  @scores[term] = RockScore.for_term(term)
+  visit query_path(:term => term)
+  score = ActiveSupport::JSON.decode(page.source).fetch("score")
+  @scores[term] = score
 end
 
 Then /^the beatles should have a higher score than comcast$/ do
